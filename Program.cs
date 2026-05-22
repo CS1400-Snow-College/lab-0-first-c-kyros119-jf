187b56c29ee83f6e2ba67f9cdf9784a11b015cc9
Jeremy <jeremy.frank@students.snow.edu>
2026-05-22 12:43:34 -0600

    finished lab 1

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string aFriend = "Anna";
+string secondfriend = "Chase";
+int aFriendage = 35;
+int secondfriendage = 33;
+Console.WriteLine($"Hello {aFriend} and {secondfriend}");
+Console.WriteLine($"My friend {secondfriend} is {secondfriendage} years old and {aFriend} is {aFriendage} years old");
